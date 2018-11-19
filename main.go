7727699991180977e522f8a94c75ae4a82d60fed
AdeoroM <adeoro@wawand.co>
2018-11-19 11:45:31 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Triangle struct {
+	CatOP int
+	CatAD int
+	Hyp   int
+	Name  string
+}
+
+func triangleInequality(tringle Triangle) bool {
+	a := tringle.CatAD
+	b := tringle.CatOP
+	h := tringle.Hyp
+	return a < (b+h) && b < (a+h) && h < (a+b)
+}
+
+func typeTriangle(tringle Triangle) string {
+	a := tringle.CatAD
+	b := tringle.CatOP
+	h := tringle.Hyp
+	name := tringle.Name
+
+	if a == b && b == h {
+		return fmt.Sprintf("Your %v triangle is Equlat", name)
+	}
+	if a == b || a == h || b == h {
+		return fmt.Sprintf("Your %v triangle is Scalene", name)
+	}
+	return fmt.Sprintf("Your %v triangle  is Isosceles", name)
+}
+
+func main() {
+
+	catOP := flag.Int("catOP", 0, "Save")
+	catAD := flag.Int("catAD", 0, "Save")
+	hyp := flag.Int("hyp", 0, "Save")
+	name := flag.String("name", "", "Save")
+	flag.Parse()
+	triangle := Triangle{
+		CatAD: *catAD,
+		CatOP: *catOP,
+		Hyp:   *hyp,
+		Name:  *name,
+	}
+	if triangleInequality(triangle) {
+		if *catAD != 0 && *catOP != 0 && *hyp != 0 {
+			if *name != "" {
+				fmt.Println(typeTriangle(triangle))
+			} else {
+				fmt.Printf("Hey, you should set the triangle name\n")
+			}
+		} else {
+			fmt.Printf("Hey, you should set the triangle parameters: catOP, catAD and hyp\n")
+		}
+	} else {
+		fmt.Printf("Your triangle is invalid\n")
+	}
+}
